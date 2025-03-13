2126c288f5c454ae191c19c4aec891148bbee33d
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-03-13 16:21:36 +0600

    Update HealthCheck.java

--- src/main/java/com/supanapi/journalApp/controller/HealthCheck.java
+++ src/main/java/com/supanapi/journalApp/controller/HealthCheck.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheck {
     @GetMapping("/health-check")
     public String healthCheck() {
-        return "OK";
+        return "0OK";
     }
 }
